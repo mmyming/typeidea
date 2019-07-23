cee945a216564ec44d4134ac8401f1865c3da99b
mmy <mmy@outlook.com>
2019-07-23 22:17:00 +0800

    views 改造

--- typeidea/config/models.py
+++ typeidea/config/models.py
@@ -43,13 +43,14 @@ class SideBar(models.Model):
     )
     status = models.PositiveIntegerField(default=1, choices=STATUS_ITEMS, verbose_name='状态')
     content = models.CharField(max_length=500,blank=True,verbose_name='内容',
-                               help_text='如果设置的不是HTML类型，可为空5')
+                               help_text='如果设置的不是HTML类型，可为空')
 
     owner = models.ForeignKey(User, verbose_name='作者')
     created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
 
     @classmethod
     def get_all(cls):
+        print(cls.objects.filter(status=1))
         return cls.objects.filter(status=1)
 
     @property
